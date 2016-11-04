7956f6a2a28aa321ce5e274c58c68403f158e0f4
Divya Balakrishna <dbalakrishna@divya-m01.vmware.com>
2016-11-04 16:27:57 -0700

    adding letters and words fetch

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 function getParaNumber(){
     document.getElementById("genText").style.display="block";
     var no = parseInt(document.getElementById("number").value);
-    genPara(no);
+    if(document.getElementById("paragraphs").checked)
+        genPara(no);
+    else if(document.getElementById("words").checked)
+        genWords(no);
+    else genLetters(no);
 }
 
 function genPara(n){
@@ -17,8 +21,37 @@ function genPara(n){
          if (i == str.length-1)
            i = 0;
          if (count < n){
-           str[i] = str[i].replace(/(\r\n|\n|\r)/gm,"");
-           output += str[i] + "\n" + "\n";
+           if(str[i])
+           {
+              str[i] = str[i].replace(/(\r\n|\n|\r)/gm,"");
+              output += str[i] + "\n" + "\n";
+              count++;
+           }
+         }
+         else break;
+       }
+
+        document.getElementById("genText").value = output;
+    }
+    client.send();
+}
+
+function genWords(n){
+    var client = new XMLHttpRequest();
+    client.open('GET', 'test.txt');
+    client.onreadystatechange = function() {
+        var n = parseInt(document.getElementById("number").value);
+        var str = client.responseText;
+        str = str.replace(/#/g," ");
+        str = str.replace(/(\r\n|\n|\r)/gm," ");
+        str = str.split(" ");
+        var output = "";
+        var count = 0;
+        for(var i=0; i<n; i++){
+         if (i == str.length-1)
+           i = 0;
+         if (count < n){
+           output += str[i] + " ";
            count++;
          }
          else break;
@@ -29,3 +62,30 @@ function genPara(n){
     client.send();
 }
 
+function genLetters(n){
+    var client = new XMLHttpRequest();
+    client.open('GET', 'test.txt');
+    client.onreadystatechange = function() {
+        var n = parseInt(document.getElementById("number").value);
+        var str = client.responseText;
+        str = str.replace(/#/g," ");
+        str = str.replace(/(\r\n|\n|\r)/gm," ");
+        str = str.split("");
+        var output = "";
+        var count = 0;
+        for(var i=0; count<n; i++){
+         //if (i == str.length-1)
+         //  i = 0;
+         if (count < n){
+           output += str[i];
+           if(str[i] && /[a-zA-Z]/.test(str[i])){
+             count++;
+           }
+         }
+         else break;
+       }
+
+        document.getElementById("genText").value = output;
+    }
+    client.send();
+}
